d900015b592c5d0d967d95d7d5ec4f37e2712f47
Opti213 <opti213@mail.ru>
2019-03-11 22:09:56 +0300

    made tasklist with listView

--- app/src/main/java/com/e/todolist_simpleexample/MainActivity.kt
+++ app/src/main/java/com/e/todolist_simpleexample/MainActivity.kt
@@ -4,8 +4,11 @@ import android.app.Activity
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.Window
+import android.widget.ArrayAdapter
 import android.widget.LinearLayout
+import android.widget.ListView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,13 +19,22 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
 
         val addBar = findViewById<LinearLayout>(R.id.addToDoLayout)
-        val container = findViewById<LinearLayout>(R.id.toDoListLayout)
+        val listV = findViewById<ListView>(R.id.listView)
+        val taskList : ArrayList<String> = ArrayList()
+        val taskAdapter : ArrayAdapter<String>
 
+        taskAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, taskList)
+        listV.adapter = taskAdapter
+        taskList.add("first task")
+        taskList.add("second task")
+
+        /* for textview
         for (i in 0..100){
             var tv = TextView(this)
             tv.text = "this $i"
             if (i % 2 == 0) tv.setBackgroundColor(Color.GRAY)
             container.addView(tv)
         }
+        */
     }
 }
