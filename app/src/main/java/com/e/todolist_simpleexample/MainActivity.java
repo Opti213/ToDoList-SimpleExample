02ddeb6255e6ca01738dcf2535d087d55757f399
Opti213 <opti213@mail.ru>
2019-03-14 18:12:44 +0300

    add remove fun

--- app/src/main/java/com/e/todolist_simpleexample/MainActivity.java
+++ app/src/main/java/com/e/todolist_simpleexample/MainActivity.java
@@ -1,13 +1,53 @@
 package com.e.todolist_simpleexample;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+    private ArrayList<String> toDoList;
+    private ArrayAdapter<String> toDoListAdapter;
+    private ListView toDoListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toDoListView = (ListView) findViewById(R.id.ToDoListView);
+        toDoList = new ArrayList<String>();
+        toDoListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toDoList);
+        toDoListView.setAdapter(toDoListAdapter);
+        setupListViewListener(); //removeListener
+        toDoList.add("First Item");
+        toDoList.add("Second Item");
+    }
+
+    public void onAddToDo(View view){
+        EditText addToDoEditText = findViewById(R.id.addToDoEditText);
+        String text = addToDoEditText.getText().toString();
+        toDoListAdapter.add(text);
+        addToDoEditText.setText("");
+    }
+
+    private void setupListViewListener() {
+        toDoListView.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        toDoList.remove(pos);
+                        toDoListAdapter.notifyDataSetChanged();
+                        return true;
+                    }
+
+                });
     }
 }
