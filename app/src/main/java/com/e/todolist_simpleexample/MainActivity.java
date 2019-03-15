3d09809727617de434bd48cac83f32a1a64a4b8c
Opti213 <opti213@mail.ru>
2019-03-15 17:38:06 +0300

    save toDoList in File(fun writeFile/readFile)

--- app/src/main/java/com/e/todolist_simpleexample/MainActivity.java
+++ app/src/main/java/com/e/todolist_simpleexample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.e.todolist_simpleexample;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,10 +11,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
-    private ArrayList<String> toDoList;
+    private ArrayList<String> toDoList = new ArrayList<>();
     private ArrayAdapter<String> toDoListAdapter;
     private ListView toDoListView;
 
@@ -21,13 +30,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readFile("toDoList");
         toDoListView = (ListView) findViewById(R.id.ToDoListView);
-        toDoList = new ArrayList<String>();
         toDoListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toDoList);
         toDoListView.setAdapter(toDoListAdapter);
         setupListViewListener(); //removeListener
-        toDoList.add("First Item");
-        toDoList.add("Second Item");
     }
 
     public void onAddToDo(View view){
@@ -35,19 +42,44 @@ public class MainActivity extends Activity {
         String text = addToDoEditText.getText().toString();
         toDoListAdapter.add(text);
         addToDoEditText.setText("");
+        writeFile("toDoList");
     }
 
     private void setupListViewListener() {
         toDoListView.setOnItemLongClickListener(
                 new AdapterView.OnItemLongClickListener() {
                     @Override
-                    public boolean onItemLongClick(AdapterView<?> adapter,
-                                                   View item, int pos, long id) {
+                    public boolean onItemLongClick(AdapterView<?> adapter, View view, int pos, long id) {
                         toDoList.remove(pos);
                         toDoListAdapter.notifyDataSetChanged();
+                        writeFile("toDoList");
                         return true;
                     }
-
                 });
+    }
+
+    void writeFile(String nameOfList) {
+        try(BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(openFileOutput(nameOfList, Context.MODE_PRIVATE)))) {
+            for (String item : toDoList){
+                bw.write(item + "\n");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void readFile(String nameOfList) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(openFileInput(nameOfList)))) {
+            String str;
+            while((str = br.readLine()) != null) {
+                toDoList.add(str);
+            }
+        } catch (FileNotFoundException e) {
+            toDoList = new ArrayList<String>();
+        } catch (IOException e) {
+            toDoList = new ArrayList<String>();
+        }
     }
 }
